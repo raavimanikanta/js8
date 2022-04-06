02eabacacc6406b6816e85421ee031f7255586cb
raavimanikanta <raavi,ani123@gmail.com>
2022-04-06 12:46:08 +0530

    first changes

---
+++ script.js
@@ -0,0 +1,41 @@
+var String ="Hello"
+var String2=String.length;
+console.log(String2)
+
+const StringChar="Hello"
+console.log(StringChar.charAt(1))
+
+
+const stringIndex="Hello"
+console.log(stringIndex.indexOf("e"))
+
+const StringReplace="Hello"
+console.log(StringReplace.replace("Hello","world"))
+
+const StringReplace1="Hello is Full of Hello" 
+console.log(StringReplace1.replaceAll("Hello","world"))
+
+const stringSplit="Hello"
+console.log(stringSplit.split(""))
+
+const stringSplit1="Javascript";
+const stringsplit2=stringSplit1.split("")
+const stringreverse=stringsplit2.reverse();
+const stringjoin=stringreverse.join("")
+console.log(stringjoin)
+
+const StirngTrim="hello   "
+const NewStringTrim=StirngTrim.trim();
+console.log(NewStringTrim.length)
+console.log(StirngTrim.length)
+
+
+const Strconcat="mani"
+const Strconcat2="kanta"
+const str3=Strconcat.concat(Strconcat2)
+console.log(str3)
+
+const StrConvert="hello";
+const StrResult=StrConvert.toUpperCase();
+console.log(StrResult)
+
